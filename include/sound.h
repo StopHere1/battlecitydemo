6a327f4cd6ae630b31a85b2d152431f9fc549af5
Qianqian Yang <ariexy0811@gmail.com>
2023-11-25 20:41:15 -0500

    separate functions for playing sound effects

--- include/sound.h
+++ include/sound.h
@@ -2,15 +2,14 @@
 #define SOUND_H
 #define GL_SILENCE_DEPRECATION
 #include <iostream>
+#include <string>
 #include "../../public/src/fssimplewindow/src/fssimplewindow.h"
 #include "../../public/src/yssimplesound/src/yssimplesound.h"
 
 class Sound
 {
-public:
-    double bgmVolumeLevel;
-    void BGM(); //load BGM and play in the background
-protected:
+private:
+    float bgmVolumeLevel;
     YsSoundPlayer bgmPlayer;
     YsSoundPlayer::SoundData bgm;
     YsSoundPlayer effectPlayer;
@@ -21,8 +20,40 @@ protected:
     YsSoundPlayer::SoundData occupy[3];
     YsSoundPlayer::SoundData toolCollected;
     YsSoundPlayer::SoundData playerStatus[2];
+protected:
     bool loadSound(YsSoundPlayer::SoundData &wav, std::string fileName);
-    void loadEffects();
+public:
+    Sound();
+    ~Sound();
+    float getBGMVolume();
+    float setBGMVolume(float input);
+
+    //load BGM and play in the background
+    void BGM(); 
+    
+    // load sound effects and start the effectPlayer
+    void loadEffects(); 
+    // play one shot of respective sound effects //
+    void playButtonClick(); // click on the menu button
+    void playLoadSelection(); // select loading for each player
+    void playSwitchPlayerSelection(); // switch to the other player after one confirmed selection
+    void playGameStart(); // pre-game selection finished and game starts
+    void playGameOver(); // winning condition matched and game over
+    void playCelebration(); // celebrate for the winner
+    void playShootBullet1(); // shoot bullet type 1
+    void playShootBullet2();
+    void playShootBullet3();
+    void playHitTank(); // when the bullet hits a tank
+    void playHitAmor(); // hits the armor and it crashed
+    void playHitProp(); // hits the prop
+    void playHitWall(); // hits the wall or block and it crashed
+    void playHitRebound(); // hits the indestructible wall or blocks
+    void playOccupying(); // when a player is occupying a point
+    void playOccupied(); // when a player successfully occupied a point
+    void playOccupyFailed(); // the occupying process is disrupted and failed
+    void playCollectTool(); // when collecting a tool from the map
+    void playDie(); // when a player dies
+    void playRevive(); // when a player revives
 };
 
 #endif //SOUND_H
--- src/sound.cpp
+++ src/sound.cpp
@@ -1,6 +1,6 @@
-#include <string>
 #include "sound.h"
 
+// protected
 bool Sound::loadSound(YsSoundPlayer::SoundData &wav, std::string fileName)
 {
     FsChangeToProgramDir();
@@ -16,14 +16,19 @@ bool Sound::loadSound(YsSoundPlayer::SoundData &wav, std::string fileName)
     }
 }
 
-void Sound::BGM()
+// pulbic
+Sound::Sound()
 {
-    // load BGM
-    std::string bgmName = "../soundlib/0bgm.wav";
-    loadSound(bgm, bgmName);
-    bgmPlayer.Start();
-    bgmPlayer.SetVolume(bgm, 0.5);
-    bgmPlayer.PlayBackground(bgm);
+    // set default bgm volume level to be 0.5
+    bgmVolumeLevel = 0.5;
+}
+
+Sound::~Sound()
+{
+    bgmVolumeLevel = 0.5;
+
+    bgmPlayer.End();
+    effectPlayer.End();
 }
 
 void Sound::loadEffects()
@@ -32,28 +37,150 @@ void Sound::loadEffects()
     loadSound(selection[0], "../soundlib/1buttonClicked.wav");
     loadSound(selection[1], "../soundlib/2loadSelected.wav");
     loadSound(selection[2], "../soundlib/3playerSwitch.wav");
-    
+
     loadSound(gameStatus[0], "../soundlib/4gameStart.wav");
-    loadSound(gameStatus[0], "../soundlib/10gameOver.wav");
+    loadSound(gameStatus[1], "../soundlib/10gameOver.wav");
     loadSound(gameStatus[2], "../soundlib/11celebration.wav");
-    
+
     loadSound(bulletShoot[0], "../soundlib/5bullet.wav");
     loadSound(bulletShoot[1], "../soundlib/5bullet2.wav");
-    loadSound(bulletShoot[1], "../soundlib/5bullet3.wav");
-    
+    loadSound(bulletShoot[2], "../soundlib/5bullet3.wav");
+
     loadSound(hit[0], "../soundlib/6hitTank.wav");
     loadSound(hit[1], "../soundlib/6hitArmor.wav");
     loadSound(hit[2], "../soundlib/6hitProp.wav");
     loadSound(hit[3], "../soundlib/6hittWall.wav");
     loadSound(hit[4], "../soundlib/6hitRebound.wav");
-    
+
     loadSound(occupy[0], "../soundlib/7occupying.wav");
     loadSound(occupy[1], "../soundlib/7occupied.wav");
     loadSound(occupy[2], "../soundlib/7occupyFailed.wav");
-    
+
     loadSound(toolCollected, "../soundlib/8toolCollected.wav");
-    
+
     loadSound(playerStatus[0], "../soundlib/9playerDie.wav");
     loadSound(playerStatus[1], "../soundlib/9playerRevive.wav");
+    effectPlayer.MakeCurrent();
     effectPlayer.Start();
+}
+
+float Sound::getBGMVolume()
+{
+    return bgmVolumeLevel;
+}
+
+float Sound::setBGMVolume(float input)
+{
+    this->bgmVolumeLevel = input;
+}
+
+void Sound::BGM()
+{
+    // load BGM
+    std::string bgmName = "../soundlib/0bgm.wav";
+    loadSound(bgm, bgmName);
+    bgmPlayer.MakeCurrent();
+    bgmPlayer.Start();
+    bgmPlayer.SetVolume(bgm, bgmVolumeLevel);
+    bgmPlayer.PlayBackground(bgm);
+}
+
+void Sound::playButtonClick()
+{
+    effectPlayer.PlayOneShot(selection[0]);
+}
+
+void Sound::playLoadSelection()
+{
+    effectPlayer.PlayOneShot(selection[1]);
+}
+
+void Sound::playSwitchPlayerSelection()
+{
+    effectPlayer.PlayOneShot(selection[2]);
+}
+
+void Sound::playGameStart()
+{
+    effectPlayer.PlayOneShot(gameStatus[0]);
+}
+
+void Sound::playGameOver()
+{
+    effectPlayer.PlayOneShot(gameStatus[1]);
+}
+
+void Sound::playCelebration()
+{
+    effectPlayer.PlayOneShot(gameStatus[2]);
+}
+
+void Sound::playShootBullet1()
+{
+    effectPlayer.PlayOneShot(bulletShoot[0]);
+}
+
+void Sound::playShootBullet2()
+{
+    effectPlayer.PlayOneShot(bulletShoot[1]);
+}
+
+void Sound::playShootBullet3()
+{
+    effectPlayer.PlayOneShot(bulletShoot[2]);
+}
+
+void Sound::playHitTank()
+{
+    effectPlayer.PlayOneShot(hit[0]);
+}
+
+void Sound::playHitAmor()
+{
+    effectPlayer.PlayOneShot(hit[1]);
+}
+
+void Sound::playHitProp()
+{
+    effectPlayer.PlayOneShot(hit[2]);
+}
+
+void Sound::playHitWall()
+{
+    effectPlayer.PlayOneShot(hit[3]);
+}
+
+void Sound::playHitRebound()
+{
+    effectPlayer.PlayOneShot(hit[4]);
+}
+
+void Sound::playOccupying()
+{
+    effectPlayer.PlayOneShot(occupy[0]);
+}
+
+void Sound::playOccupied()
+{
+    effectPlayer.PlayOneShot(occupy[1]);
+}
+
+void Sound::playOccupyFailed()
+{
+    effectPlayer.PlayOneShot(occupy[2]);
+}
+
+void Sound::playCollectTool()
+{
+    effectPlayer.PlayOneShot(toolCollected);
+}
+
+void Sound::playDie()
+{
+    effectPlayer.PlayOneShot(playerStatus[0]);
+}
+
+void Sound::playRevive()
+{
+    effectPlayer.PlayOneShot(playerStatus[1]);
 }
